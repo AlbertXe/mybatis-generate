a97a4046661be4cbc6b53cb98bc8710b5e8167d2
AlbertXe <563000409@qq.com>
2021-07-14 23:28:07 +0800

    init

---
+++ readme.md
@@ -0,0 +1,3 @@
+地址
+https://www.cnblogs.com/personsiglewine/p/12848595.html
+
---
+++ src/main/java/com/demo/User.java
@@ -0,0 +1,55 @@
+package com.demo;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private Date updateTime;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
